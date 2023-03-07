b44b4b7f7bef91d870155453b370df132126e726
Claudio Pi <claudiopi@gmail.com>
2023-03-07 16:52:36 -0300

    Task Management updates

--- src/app/kanban-view/kanban-view.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@open-wc/testing';
-import KanbanView from './kanban-view.js';
-
-describe('KanbanView', () => {
-  it('<app-kanban-view> is an instance of KanbanView', async () => {
-    const element = document.createElement('app-kanban-view');
-    expect(element).to.be.instanceOf(KanbanView);
-  });
-});
--- src/app/list-view/list-view.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@open-wc/testing';
-import ListView from './list-view.js';
-
-describe('ListView', () => {
-  it('<app-list-view> is an instance of ListView', async () => {
-    const element = document.createElement('app-list-view');
-    expect(element).to.be.instanceOf(ListView);
-  });
-});
